b89c715cb7ec519d501401d5b55d8a54ada529f3
Adam Elaoud <adam.elaoud@gmail.com>
2020-11-25 21:08:13 +0000

    added main menu assets and completed main menu layout

--- Apollo/app/src/main/java/com/example/apollo/Menu.kt
+++ Apollo/app/src/main/java/com/example/apollo/Menu.kt
@@ -2,10 +2,21 @@ package com.example.apollo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // launch Stats Activity
+    fun onClickStats(v: View) {
+
+    }
+
+    // launch Play Activity
+    fun onClickPlay(v: View) {
+
     }
 }
